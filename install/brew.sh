55a89b1522fc24490174f9f4f08e46732ec7e96a
Edward Chu <crazyzyt@gmail.com>
2017-04-01 19:48:04 +0800

    use homebrew mirror

--- install/brew.sh
+++ install/brew.sh
@@ -7,19 +7,29 @@ fi
 
 echo "Installing homebrew packages..."
 
+# use Tsinghua mirror to speed up the process
+# homebrew-core
+cd "$(brew --repo)"
+git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+# homebrew-bottles
+echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles' >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
 # cli tools
-brew install ack
-brew install tree
-brew install wget
+brew reinstall ack
+brew reinstall tree
+brew reinstall wget
 
 # development server setup
-brew install nginx
+brew reinstall nginx
 
 # development tools
-brew install git
-brew install autojump
-brew install reattach-to-user-namespace
-brew install tmux
-brew install zsh
-brew install highlight
-# brew install macvim --with-override-system-vim
+brew reinstall git
+brew reinstall autojump
+brew reinstall reattach-to-user-namespace
+brew reinstall tmux
+brew reinstall zsh
+brew reinstall highlight
+# brew reinstall macvim --with-override-system-vim
