f87d52767bd89266f865c602443c212631a68cf9
Edward Chu <crazyzyt@gmail.com>
2017-07-23 12:36:56 +0800

    use mirrors

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,23 +1,33 @@
 DOTFILES=$HOME/dotfiles
 export ZSH=$HOME/.oh-my-zsh
 
+# --------
 # Plugins
+# --------
 
 ## Autojump
 [[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
 
 ## Oh-my-zsh
-## Other available options: sublime / atom / ruby
+## Available options: sublime atom ruby
 plugins=(git osx autojump brew history dirhistory nvm-auto-switch zsh-better-npm-completion zsh-autosuggestions)
 
-
+# --------
 # Mirrors
+# --------
+export ELECTRON_MIRROR=https://npm.taobao.org/mirrors/electron/        # Electron
+export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/dist               # nvm
+export PHANTOMJS_CDNURL=https://npm.taobao.org/dist/phantomjs          # phantomjs
+export SASS_BINARY_SITE=https://npm.taobao.org/mirrors/node-sass/      # node-sass
+export CHROMEDRIVER_CDNURL=http://npm.taobao.org/mirrors/chromedriver  # ChromeDriver
+export SELENIUM_CDNURL=http://npm.taobao.org/mirrorss/selenium         # Selenium
 
-## Electron
-export ELECTRON_MIRROR=https://npm.taobao.org/mirrors/electron/
 
-# Theme: Bullet Train
-## https://github.com/caiogondim/bullet-train-oh-my-zsh-theme
+# --------
+# Theme:
+#   Bullet Train
+#   https://github.com/caiogondim/bullet-train-oh-my-zsh-theme
+# --------
 ZSH_THEME="bullet-train"
 export BULLETTRAIN_DIR_FG=black
 export BULLETTRAIN_DIR_EXTENDED=2
