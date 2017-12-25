1e452c873abec68ce1ec3514eba54f0c6a2f5539
Edward Chu <crazyzyt@gmail.com>
2017-12-25 15:48:49 +0800

    add nvm-iojs-mirror

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -17,6 +17,7 @@ plugins=(git osx autojump brew history dirhistory nvm-auto-switch zsh-better-npm
 # --------
 export ELECTRON_MIRROR=https://npm.taobao.org/mirrors/electron/        # Electron
 export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/dist               # nvm
+export NVM_IOJS_ORG_MIRROR=https://npm.taobao.org/mirrors/iojs         # nvm-iojs
 export PHANTOMJS_CDNURL=https://npm.taobao.org/dist/phantomjs          # phantomjs
 export SASS_BINARY_SITE=https://npm.taobao.org/mirrors/node-sass/      # node-sass
 export CHROMEDRIVER_CDNURL=http://npm.taobao.org/mirrors/chromedriver  # ChromeDriver
