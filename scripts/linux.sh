c53bd9d23d1c6d137e3341fb1ee6e597f6391159
Edward Chu <edc@edc-MacBook-Pro.local>
2018-05-05 23:19:15 +0800

    fix: autojump bug

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -5,4 +5,4 @@ wget -O /usr/local/bin/pacapt https://github.com/icy/pacapt/raw/ng/pacapt
 chmod 755 /usr/local/bin/pacapt
 ln -sv /usr/local/bin/pacapt /usr/local/bin/pacman || true
 
-pacapt -S --noconfirm autojump
+pacapt -S --noconfirm autojump-zsh
