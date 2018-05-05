d44bad9a28f68877a79f0da673ec2cc4b0c76828
Edward Chu <edc@edc-MacBook-Pro.local>
2018-05-05 23:35:09 +0800

    add vim-plug

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -5,4 +5,9 @@ wget -O /usr/local/bin/pacapt https://github.com/icy/pacapt/raw/ng/pacapt
 chmod 755 /usr/local/bin/pacapt
 ln -sv /usr/local/bin/pacapt /usr/local/bin/pacman || true
 
+# install autojump-zsh
 pacapt -S --noconfirm autojump-zsh
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
